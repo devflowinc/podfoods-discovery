7d0d763abdc0d918b779a1aca843ef3b763811cb
danielsgriffin <danielsgriffin@gmail.com>
2024-08-15 16:45:50 -0700

    feat: add license; add links to repo and blogpost in header

--- frontend/app/components/CatalogHeaderBar.vue
+++ frontend/app/components/CatalogHeaderBar.vue
@@ -14,19 +14,12 @@ const toggleSearch = () => {
 
 <template>
     <div class="catalog-header-bar">
-        <div id="top-header-bar" 
-             :class="['top', 'flex', 'inner-gap', { searching: isSearching }]"
-             data-v-4ac4b396="" 
-             style="background: rgb(253, 244, 255); align-items: center;">    
-            <div
-                class="logo"
-                data-v-4ac4b396=""
-                style="margin: 5px; margin-right: 30px; padding-right: 10px; background-color: white; border-radius: 8px; max-width: 200px;"
-                >
-                <a href="/" title="Trieve Search Demo - Pod Foods" class="nuxt-link-active"
-                data-v-4ac4b396="">
-                    <img src="/img/trieve-podfoods-logo.png" data-v-4ac4b396=""
-                         style="width: 100%; height: auto;">
+        <div id="top-header-bar" :class="['top', 'flex', 'inner-gap', { searching: isSearching }]" data-v-4ac4b396=""
+            style="background: rgb(253, 244, 255); align-items: center;">
+            <div class="logo" data-v-4ac4b396=""
+                style="margin: 5px; margin-right: 30px; padding-right: 10px; background-color: white; border-radius: 8px; max-width: 200px;">
+                <a href="/" title="Trieve Search Demo - Pod Foods" class="nuxt-link-active" data-v-4ac4b396="">
+                    <img src="/img/trieve-podfoods-logo.png" data-v-4ac4b396="" style="width: 100%; height: auto;">
                 </a>
             </div>
             <div class="search" data-v-4ac4b396="" style="flex-grow: 1; justify-content: center;">
@@ -34,19 +27,39 @@ const toggleSearch = () => {
                     <SearchBox :isFocused="isFocused" />
                 </div>
             </div>
-            <a href="#" class="search-toggler" data-v-4ac4b396="" style="margin-left: auto;" @click.prevent="toggleSearch">
-                <i :class="['pf-icon', 'bx', { 'bx-search': !isSearching, 'bx-x': isSearching }]" data-v-4ac4b396=""></i>
+            <a href="#" class="search-toggler" data-v-4ac4b396="" style="margin-left: auto;"
+                @click.prevent="toggleSearch">
+                <i :class="['pf-icon', 'bx', { 'bx-search': !isSearching, 'bx-x': isSearching }]"
+                    data-v-4ac4b396=""></i>
             </a>
+            <div 
+                style="height: auto; border: 1px solid rgb(217 70 239); border-radius: 5px; margin: 2px; padding: 2px; background-color: rgb(253 244 255); display: flex; flex-direction: column;"
+                >
+                <a href="https://github.com/devflowinc/podfoods-discovery/" 
+                   title="GitHub" 
+                   data-v-4ac4b396=""
+                   class="hover-link">
+                    <i class="bx bxl-github mr-1"></i>
+                    <span class="font-semibold">GitHub</span>
+                </a>
+                <a href="https://trieve.ai/podfoods-relevance-blog/" 
+                   title="Blogpost: Search Relevance Exploration"
+                   data-v-4ac4b396=""
+                   class="hover-link"
+                   >
+                    <i class="bx bx-book mr-1"></i>
+                    <span class="font-semibold">Blogpost</span>
+                </a>
+            </div>
         </div>
-        
-        <div class="bottom flex inner-gap" data-v-4ac4b396="">
-                    <div class="expander" data-v-4ac4b396=""></div>
-                    <div class="menu" data-v-4ac4b396="">
-                        <a href="/" title="Catalog" class="nuxt-link-active" data-v-4ac4b396="">Catalog</a>
-                        <a href="/" title="Brands" data-v-4ac4b396="" class="">Brands
-                        </a>
-                    </div>
-                </div>
-        <CategoriesBar/>
+
+        <div class="bottom flex" data-v-4ac4b396="">
+            <div class="expander" data-v-4ac4b396=""></div>
+            <div class="menu" data-v-4ac4b396="">
+                <a href="/" title="Catalog" class="nuxt-link-active" data-v-4ac4b396="">Catalog</a>
+                <a href="/" title="Brands" data-v-4ac4b396="" class="">Brands</a>
+            </div>
+        </div>
+        <CategoriesBar />
     </div>
 </template>
--- frontend/app/components/CategoriesBar.vue
+++ frontend/app/components/CategoriesBar.vue
@@ -2,49 +2,62 @@
     <div class="categories-bar" data-v-35c34d0d="" data-v-4ac4b396="">
         <div class="scroller" data-v-35c34d0d="">
             <div class="categories" data-v-35c34d0d="">
-                <div class="categories" data-v-35c34d0d="">
-                    <a href="/" title="All" class="category category--all active" data-v-35c34d0d="">
-                        <div class="icon icon--all" data-v-35c34d0d=""></div>
-                        <span class="name pf-ellipsis" data-v-35c34d0d="">All</span></a>
-                    <a href="/" title="New" class="category category--new" data-v-35c34d0d="">
-                        <div class="icon icon--new" data-v-35c34d0d=""></div>
-                        <span class="name pf-ellipsis" data-v-35c34d0d="">New</span></a>
-                    <a href="/" title="Grocery" class="category" data-v-35c34d0d="">
-                        <div class="icon icon--grocery" data-v-35c34d0d=""></div>
-                        <span class="name pf-ellipsis" data-v-35c34d0d="">Grocery</span></a>
-                    <a href="/" title="Frozen" class="category" data-v-35c34d0d="">
-                        <div class="icon icon--frozen" data-v-35c34d0d=""></div>
-                        <span class="name pf-ellipsis" data-v-35c34d0d="">Frozen</span></a>
-                    <a href="/" title="Beverage" class="category" data-v-35c34d0d="">
-                        <div class="icon icon--beverage" data-v-35c34d0d=""></div>
-                        <span class="name pf-ellipsis" data-v-35c34d0d="">Beverage</span></a>
-                    <a href="/" title="Bakery" class="category" data-v-35c34d0d="">
-                        <div class="icon icon--bakery" data-v-35c34d0d=""></div>
-                        <span class="name pf-ellipsis" data-v-35c34d0d="">Bakery</span></a>
-                    <a href="/" title="Confections" class="category" data-v-35c34d0d="">
-                        <div class="icon icon--confections" data-v-35c34d0d=""></div>
-                        <span class="name pf-ellipsis" data-v-35c34d0d="">Confections</span></a>
-                    <a href="/" title="Household" class="category" data-v-35c34d0d="">
-                        <div class="icon icon--household" data-v-35c34d0d=""></div>
-                        <span class="name pf-ellipsis" data-v-35c34d0d="">Household</span></a>
-                    <a href="/" title="Snacks" class="category" data-v-35c34d0d="">
-                        <div class="icon icon--snacks" data-v-35c34d0d=""></div>
-                        <span class="name pf-ellipsis" data-v-35c34d0d="">Snacks</span></a>
-                    <a href="/" title="Baby &amp; Child" class="category" data-v-35c34d0d="">
-                        <div class="icon icon--baby-child" data-v-35c34d0d=""></div>
-                        <span class="name pf-ellipsis" data-v-35c34d0d="">Baby &amp; Child</span></a>
-                    <a href="/" title="Chill" class="category" data-v-35c34d0d="">
-                        <div class="icon icon--chill" data-v-35c34d0d=""></div>
-                        <span class="name pf-ellipsis" data-v-35c34d0d="">Chill</span></a>
-                    <a href="/" title="Meat" class="category" data-v-35c34d0d="">
-                        <div class="icon icon--meat" data-v-35c34d0d=""></div>
-                        <span class="name pf-ellipsis" data-v-35c34d0d="">Meat</span></a>
-                    <a href="/" title="Hemp" class="category" data-v-35c34d0d="">
-                        <div class="icon icon--hemp" data-v-35c34d0d=""></div>
-                        <span class="name pf-ellipsis" data-v-35c34d0d="">Hemp</span></a>
-                    <a href="/" title="Wellness" class="category" data-v-35c34d0d="">
-                        <div class="icon icon--wellness" data-v-35c34d0d=""></div>
-                        <span class="name pf-ellipsis" data-v-35c34d0d="">Wellness</span></a></div>
+                <a href="/" title="All" class="category category--all active" data-v-35c34d0d="">
+                    <div class="icon icon--all" data-v-35c34d0d=""></div>
+                    <span class="name pf-ellipsis" data-v-35c34d0d="">All</span>
+                </a>
+                <a href="/" title="New" class="category category--new" data-v-35c34d0d="">
+                    <div class="icon icon--new" data-v-35c34d0d=""></div>
+                    <span class="name pf-ellipsis" data-v-35c34d0d="">New</span>
+                </a>
+                <a href="/" title="Grocery" class="category" data-v-35c34d0d="">
+                    <div class="icon icon--grocery" data-v-35c34d0d=""></div>
+                    <span class="name pf-ellipsis" data-v-35c34d0d="">Grocery</span>
+                </a>
+                <a href="/" title="Frozen" class="category" data-v-35c34d0d="">
+                    <div class="icon icon--frozen" data-v-35c34d0d=""></div>
+                    <span class="name pf-ellipsis" data-v-35c34d0d="">Frozen</span>
+                </a>
+                <a href="/" title="Beverage" class="category" data-v-35c34d0d="">
+                    <div class="icon icon--beverage" data-v-35c34d0d=""></div>
+                    <span class="name pf-ellipsis" data-v-35c34d0d="">Beverage</span>
+                </a>
+                <a href="/" title="Bakery" class="category" data-v-35c34d0d="">
+                    <div class="icon icon--bakery" data-v-35c34d0d=""></div>
+                    <span class="name pf-ellipsis" data-v-35c34d0d="">Bakery</span>
+                </a>
+                <a href="/" title="Confections" class="category" data-v-35c34d0d="">
+                    <div class="icon icon--confections" data-v-35c34d0d=""></div>
+                    <span class="name pf-ellipsis" data-v-35c34d0d="">Confections</span>
+                </a>
+                <a href="/" title="Household" class="category" data-v-35c34d0d="">
+                    <div class="icon icon--household" data-v-35c34d0d=""></div>
+                    <span class="name pf-ellipsis" data-v-35c34d0d="">Household</span>
+                </a>
+                <a href="/" title="Snacks" class="category" data-v-35c34d0d="">
+                    <div class="icon icon--snacks" data-v-35c34d0d=""></div>
+                    <span class="name pf-ellipsis" data-v-35c34d0d="">Snacks</span>
+                </a>
+                <a href="/" title="Baby &amp; Child" class="category" data-v-35c34d0d="">
+                    <div class="icon icon--baby-child" data-v-35c34d0d=""></div>
+                    <span class="name pf-ellipsis" data-v-35c34d0d="">Baby &amp; Child</span>
+                </a>
+                <a href="/" title="Chill" class="category" data-v-35c34d0d="">
+                    <div class="icon icon--chill" data-v-35c34d0d=""></div>
+                    <span class="name pf-ellipsis" data-v-35c34d0d="">Chill</span>
+                </a>
+                <a href="/" title="Meat" class="category" data-v-35c34d0d="">
+                    <div class="icon icon--meat" data-v-35c34d0d=""></div>
+                    <span class="name pf-ellipsis" data-v-35c34d0d="">Meat</span>
+                </a>
+                <a href="/" title="Hemp" class="category" data-v-35c34d0d="">
+                    <div class="icon icon--hemp" data-v-35c34d0d=""></div>
+                    <span class="name pf-ellipsis" data-v-35c34d0d="">Hemp</span>
+                </a>
+                <a href="/" title="Wellness" class="category" data-v-35c34d0d="">
+                    <div class="icon icon--wellness" data-v-35c34d0d=""></div>
+                    <span class="name pf-ellipsis" data-v-35c34d0d="">Wellness</span>
+                </a>
             </div>
         </div>
     </div>
